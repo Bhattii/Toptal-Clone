f0172dad5e89338593b08253b08ffeb58b7e2ebb
Muhammad Imran <imran5244513@gamil.com>
2024-05-25 14:18:55 -0700

    update

--- src/components/BlogHeader.jsx
+++ src/components/BlogHeader.jsx
@@ -1,17 +1,33 @@
 /** @format */
 import blogbg from '../assets/img/Certificates/BlogBg.svg';
-
 export const BlogHeader = () => {
-	<>
-		<div className='flex justify-center items-center'>
-			<img
-				src={blogbg}
-				alt='background'
-				className='w-full h-full'
-			/>
-			<div>
-				<h2 className='text-4xl font-light'>Toptal Blog</h2>
+	return (
+		<>
+			<div className=''>
+				<img
+					src={blogbg}
+					alt=''
+					className='object-cover relative'
+				/>
+				<div className='absolute top-0 text-white p-11 max-w-[1200px]'>
+					<div className='p-11 max-w'>
+						<h2 className='text-6xl font-light pb-3'>Toptal Blog</h2>
+						<p className='py-3'>
+							The Toptal Blog is the top hub for developers, designers, finance
+							experts, executives, and entrepreneurs, featuring key technology
+							updates, tutorials, freelancer resources, and management insights.
+						</p>
+						<div className='flex'>
+							<button></button>
+							<span>132.3k</span>
+							<span className='uppercase'>shares</span>
+						</div>
+						<div>
+							<h2>Featured In</h2>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</>;
+		</>
+	);
 };
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,13 +1,20 @@
 /** @format */
 
 import { BlogHeader } from '../components/BlogHeader';
+// import Footer from '../layouts/Footer';
 import { Header } from '../layouts/Header';
 
 export const Blog = () => {
 	return (
 		<>
-			<Header />
-			<BlogHeader />
+			<div className=''>
+				<Header />
+			</div>
+			<div className='relative top-16'>
+				<BlogHeader />
+			</div>
+
+			{/* <Footer /> */}
 		</>
 	);
 };
