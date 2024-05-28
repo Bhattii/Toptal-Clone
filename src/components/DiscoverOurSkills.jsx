131df080773808efc3c67b246035fe67550c369e
Muhammad Imran <imran5244513@gamil.com>
2024-05-28 00:31:14 -0700

    update

--- src/components/DiscoverOurSkills.jsx
+++ src/components/DiscoverOurSkills.jsx
@@ -1,106 +1,108 @@
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+/** @format */
+
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function DiscoverOurSkills() {
-  return (
-    <>
-      <div className="bg-gray-100 p-20 flex justify-center items-center overflow-hidden">
-        <div className="max-w-[1200px]">
-          <div className=" flex flex-col justify-center items-center my-4 ">
-            <h2 className="text-5xl text-gray-900 font-semibold text-center">
-              Discover Our Expert Skillsets
-            </h2>
-          </div>
-          <div className="bg-white shadow-2xl mt-20 py-3 border  font-semibold grid grid-cols-1 md:grid-cols-2 md:gap-11 ">
-            <div className="pr-6">
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-            </div>
-            <div className="pr-6">
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-              <div className="p-4 mx-4 flex justify-between items-center hover:text-blue-800 w-full">
-                <a href="">Web Development</a>
-                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className='bg-gray-100 p-10 lg:p-20 flex justify-center items-center overflow-hidden'>
+				<div className='max-w-[1200px] w-full'>
+					<div className=' flex flex-col justify-center items-center my-4 '>
+						<h2 className=' text-3xl sm:text-5xl text-gray-900 font-semibold text-center w-full'>
+							Discover Our Expert Skillsets
+						</h2>
+					</div>
+					<div className='bg-white shadow-2xl mt-20 py-3 border w-full font-semibold grid grid-cols-1 md:grid-cols-2 md:gap-11 '>
+						<div className='pr-6'>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+						</div>
+						<div className='pr-6'>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 border-b flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+							<div className='p-4 mx-4 flex justify-between items-center hover:text-blue-800 w-full'>
+								<a href=''>Web Development</a>
+								<FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default DiscoverOurSkills;
